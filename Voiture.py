f4a83af39bbdb1d4d1ea1074fc0d4da23ed6e190
stone42-upload <arielstone883@gmail.com>
2026-03-08 08:53:32 -0400

    Creation de la classe Voiture et de ses attributs

---
+++ Voiture.py
@@ -0,0 +1,7 @@
+class Voiture:
+    def __init__(self, matricule, annee, marque, kilometrage, chauffeur=None):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = chauffeur
