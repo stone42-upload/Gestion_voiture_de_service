391de3e28448507bae4e8143f1ba193726dba644
stone42-upload <arielstone883@gmail.com>
2026-03-08 09:39:03 -0400

    Le programme main affiche suite à ce commit les informations de tous les obets créer

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ Employe1 = Employe("A1234-56783-12845", "Georges", "Winteral")
 Employe2 = Employe("B5837-97832-67249", "Harry", "Potter")
 Employe3 = Employe("F5482-75954-78135", "Luis", "Nova")
 
-Voiture1 = Voiture("ABCJ 123", 2020,"volswagen", 45000 )
+Voiture1 = Voiture("ABCJ 123", 2020,"volswagen", 45000)
 Voiture2= Voiture("123 ABC", 2018, "Honda", 78000)
 Voiture3 = Voiture("KLM-9087", 2023, "Ford", 15000)
 
 Employe1.afficherinformation()
 Employe2.afficherinformation()
 Employe3.afficherinformation()
+
+Voiture1.afficherinformation()
+Voiture2.afficherinformation()
+Voiture3.afficherinformation()
