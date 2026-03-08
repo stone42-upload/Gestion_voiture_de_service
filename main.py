09886564d9cae43894f91dfcead3ca787a6ffbd6
stone42-upload <arielstone883@gmail.com>
2026-03-08 09:24:33 -0400

    Creation du programme principal et creation de plusieurs instances de la classe Voiture et Employe

---
+++ main.py
@@ -0,0 +1,10 @@
+from Employe import Employe
+from Voiture import Voiture
+
+Emloye1 = Employe("A1234-56783-12845", "Georges", "Winteral")
+Emloye2 = Employe("B5837-97832-67249", "Harry", "Potter")
+Emloye3 = Employe("F5482-75954-78135", "Luis", "Nova")
+
+Voiture1 = Voiture("ABCJ 123", 2020,"volswagen", 45000 )
+Voiture2= Voiture("123 ABC", 2018, "Honda", 78000)
+Voiture3 = Voiture("KLM-9087", 2023, "Ford", 15000)
